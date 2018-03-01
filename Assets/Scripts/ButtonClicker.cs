c0bf04a6d85581914ea3e0d78406d67c1cadc065
Unknown <okh216@nyu.edu>
2018-03-01 15:39:01 -0500

    Working on Buttonclicker script
    Why doesn't "OnMouseDown" work as a function?

---
+++ Assets/Scripts/ButtonClicker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClicker : MonoBehaviour {
+
+    public GameObject
+    // Use this for initialization
+    void Start() {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+    void OnMouseDown () {
+
+    }
